9ed8720d91a15a49791b79bbb8d29d23de2a446f
Surprise <sabraboi@gmail.com>
2024-01-19 18:05:06 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,5 +17,4 @@ printf("size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
 printf("size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
 printf("size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 return (0);
-}	
-
+}
